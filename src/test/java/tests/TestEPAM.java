e1fb9fbe5fb610eeadd5f21fd28d58b3e7cd974a
doreshin <d-190-61-95@yandex.ru>
2021-08-15 15:45:41 +0300

    adds parallel execution by testng.xml

--- src/test/java/tests/TestEPAM.java
+++ src/test/java/tests/TestEPAM.java
@@ -4,6 +4,7 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -35,6 +36,7 @@ public class TestEPAM extends Setup {
     @Story("View upcoming events")
     @Description("Test view upcoming events and comparing counter value with real count of events cards")
     public void testViewUpcomingEvents() {
+        WebDriver driver = startUp();
         MainPage mainPage = new MainPage(driver);
         EventsPage eventsPage = new EventsPage(driver);
         int countOfEventsOnPage = mainPage
@@ -43,12 +45,14 @@ public class TestEPAM extends Setup {
                 .getCountOfEventsOnPage();
         int counterValue = eventsPage.getUpcomingEventsCounterValue();
         Assert.assertEquals(countOfEventsOnPage, counterValue, "Cards quantity does not match!");
+        closeUp(driver);
     }
 
     @Test(description = "View past events")
     @Story("View past events")
     @Description("Test view past events and validate event card's info")
     public void testViewPastEvents() {
+        WebDriver driver = startUp();
         MainPage mainPage = new MainPage(driver);
         List<String> cardInfoList = mainPage
                 .open()
@@ -56,12 +60,14 @@ public class TestEPAM extends Setup {
                 .pastEventClick()
                 .getCardInfo(testCardNumber);
         Assert.assertEquals(cardInfoList.size(), 5, "There is not enough information on the card!");
+        closeUp(driver);
     }
 
     @Test(description = "Upcoming events date")
     @Story("View upcoming events")
     @Description("Test events date")
     public void testEventDate() {
+        WebDriver driver = startUp();
         MainPage mainPage = new MainPage(driver);
         EventsPage eventsPage = new EventsPage(driver);
         List<WebElement> cardList = mainPage
@@ -69,12 +75,14 @@ public class TestEPAM extends Setup {
                 .goToEventsPage()
                 .getListOfCards();
         Assert.assertTrue(EventsPage.checkDate(EventsPage.parseEventDate(eventsPage.getEventDate(cardList.get(0)))));
+        closeUp(driver);
     }
 
     @Test(description = "Search events by location")
     @Story("Search events")
     @Description("Search past events by location and validate event info")
     public void testEventLocation() {
+        WebDriver driver = startUp();
         MainPage mainPage = new MainPage(driver);
         EventsPage eventsPage = new EventsPage(driver);
         int countOfEventsOnPage = mainPage
@@ -87,12 +95,14 @@ public class TestEPAM extends Setup {
         Assert.assertEquals(countOfEventsOnPage, counterValue, "Cards quantity does not match!");
         LocalDate[] dates = EventsPage.parseEventDate(eventsPage.getEventDate(eventsPage.getListOfCards().get(0)));
         Assert.assertTrue(dates[1].isBefore(LocalDate.now()));
+        closeUp(driver);
     }
 
     @Test(description = "Filtering events by several parameters")
     @Story("Search talks")
     @Description("Filtering talks by several parameters and validate event info")
     public void testFilterEvent() {
+        WebDriver driver = startUp();
         MainPage mainPage = new MainPage(driver);
         TalkCardPage talkCardPage = mainPage.open()
                 .goToVideoPage()
@@ -107,18 +117,21 @@ public class TestEPAM extends Setup {
         Assert.assertTrue(talkLocation.contains(testLocationBelarus));
         Assert.assertTrue(talkLanguage.contains(testLanguage));
         Assert.assertTrue(talkCategories.contains(testTopic));
+        closeUp(driver);
     }
 
     @Test(description = "Search talks")
     @Story("Search talks")
     @Description("Test talks search")
     public void testEventSearch() {
+        WebDriver driver = startUp();
         MainPage mainPage = new MainPage(driver);
         Assert.assertTrue(mainPage
                 .open()
                 .goToVideoPage()
                 .searchEvents(testSearchValue)
                 .checkCardsName(testSearchValue), "Card names do not match search value!");
+        closeUp(driver);
     }
 
 
--- src/test/java/utilities/Setup.java
+++ src/test/java/utilities/Setup.java
@@ -3,32 +3,26 @@ package utilities;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
 public class Setup {
 
     private final Logger logger = LogManager.getLogger(Setup.class);
-    protected WebDriver driver;
 
-    @BeforeClass(description = "Initialize web driver")
-    public void startUp() {
-        driver = WebDriverFactory.createDriver(WebDriverType.CHROME);
+    @BeforeMethod(description = "Initialize web driver")
+    public WebDriver startUp() {
+        WebDriver driver = WebDriverFactory.createDriver(WebDriverType.CHROME);
         //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        //driver.manage().window().maximize();
+        driver.manage().window().maximize();
         logger.info("Driver initialized!");
+        return driver;
     }
 
-    @AfterClass(description = "Close web driver")
-    public void closeUp() {
+    @AfterMethod(description = "Close web driver")
+    public void closeUp(WebDriver driver) {
         if (driver != null) driver.quit();
         logger.info("Driver closed!");
-    }
-
-    @AfterMethod(description = "Clean browser cookies")
-    public void clean() {
-        driver.manage().deleteAllCookies();
     }
 
 }
